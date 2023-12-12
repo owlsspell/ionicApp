a4eba039e5b2cedc17630fd27f466c95d55981d1
owlsspell <arhipowael@gmail.com>
2023-12-12 17:46:19 +0200

    first commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
 import './ExploreContainer.css';
+import { IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, useIonViewDidEnter } from '@ionic/react';
+import axios from 'axios';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [users, setUsers] = useState<any>([])
+  useIonViewDidEnter(() => {
+    axios.get('https://randomuser.me/api/?results=100').then((response) => setUsers(response.data.results)
+    )
+  }, [])
+
+  function deleteItem(email: string) {
+    setUsers(users.filter(user => (user.email !== email)))
+  }
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+
+      <IonList>
+        {users.length > 0 ? users.map((user) => {
+          return (<IonItemSliding key={user.id.value + user.name.first + user.name.last}>
+            <IonItem >
+              <IonLabel> {user.name.first} {user.name.last} </IonLabel>
+            </IonItem>
+            <IonItemOptions>
+              <IonItemOption color="danger" onClick={() => deleteItem(user.email)}>Delete</IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>)
+        }) : ""}
+
+      </IonList>
+
+    </div >
   );
 };
 
